74692bb129d5ac8f94dbf09fa7982561092de96e
Matthew Schwarz <schwarz1016@gmail.com>
2018-03-14 23:21:56 -0400

    current progress

---
+++ app/src/main/java/JsonUtils.java
@@ -0,0 +1,22 @@
+import android.util.Log;
+
+import org.json.JSONObject;
+
+/**
+ * Created by matthewschwarz on 3/14/18.
+ */
+
+public class JsonUtils {
+
+    public static JSONObject parseStockJson(String json){
+        JSONObject quote = null;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            quote = jsonObject.getJSONObject("quote");
+            Log.i("JSON", quote.toString());
+        }catch (Exception e){
+            Log.e("JSON", "Error parsing JSON");
+        }
+        return quote;
+    }
+}
